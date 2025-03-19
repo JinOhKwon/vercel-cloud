fed043b72e723764e5299e2087a20111d98792fc
JinOhKwon <kjo1333@gmail.com>
2025-03-19 17:22:43 +0900

    ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -10,11 +10,14 @@ interface ButtonProps {
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
+    <>
+      <h1>UI!!!!!!!!!!!!!!!</h1>
+      <button
+        className={className}
+        onClick={() => alert(`Hello from your ${appName} app!`)}
+      >
+        {children}
+      </button>
+    </>
   );
 };
